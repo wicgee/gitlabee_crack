f0aacde7fbe22aca4f522fc3730c179d31d9d418
Wicgee <chenweijiSun@126.com>
2026-04-07 14:28:58 +0800

    Update shared memory size and display root password
    Increase shared memory size for GitLab EE container and output initial root password.

--- deploy_gitlabee.sh
+++ deploy_gitlabee.sh
@@ -9,6 +9,6 @@ docker run --detach \
   --volume $GITLAB_HOME/config:/etc/gitlab \
   --volume $GITLAB_HOME/logs:/var/log/gitlab \
   --volume $GITLAB_HOME/data:/var/opt/gitlab \
+  --shm-size 512m \
   gitlab-ee:crack
-  # --shm-size 256m \
-# cat $GITLAB_HOME/config/initial_root_password
+cat $GITLAB_HOME/config/initial_root_password
